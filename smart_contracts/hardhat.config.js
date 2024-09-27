bf62702f64a3aca55d45efe423f69ba0db0b6ea8
Sughter Tooyosi Achi <88914823+Rockranx@users.noreply.github.com>
2024-09-27 21:03:32 +0100

    Add files via upload

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -6,9 +6,9 @@ require ('@nomiclabs/hardhat-waffle');
 module.exports= {
   solidity:  '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/6a0PSVr4MZDE2Fax3M9c2DiMofmQWlYp',
-      accounts: ['4ade36c90e32870781b74aed6c6d09c8ec9184474219b97ac2c12fa43a31baac']
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/GKzGEwRAy7n1RAqzho3yDC6KNGEr6b6l",
+      accounts: ['bce634e0ae139a39e5f84190817e5ba63a90ef6e8b40f5415b3eb2bcb286cc5d']
     }
   }
 }

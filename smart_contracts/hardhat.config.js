ccc67b70dd00e5b841f22bc9ebe91758af3d9361
Sughter Tooyosi Achi <88914823+Rockranx@users.noreply.github.com>
2024-09-27 21:07:26 +0100

    Update hardhat.config.js

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -6,9 +6,10 @@ require ('@nomiclabs/hardhat-waffle');
 module.exports= {
   solidity:  '0.8.0',
   networks: {
+    
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/GKzGEwRAy7n1RAqzho3yDC6KNGEr6b6l",
-      accounts: ['bce634e0ae139a39e5f84190817e5ba63a90ef6e8b40f5415b3eb2bcb286cc5d']
+      url:"", // use alchemy api here
+      accounts: [''] // use wallet private key
     }
   }
 }
